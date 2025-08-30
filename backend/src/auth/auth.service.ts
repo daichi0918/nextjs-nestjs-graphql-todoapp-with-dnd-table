7bfabb57aec3ee62da441e2baa08bfbde220d3df
daichi0918 <kayasugadaichi@icloud.com>
2025-08-30 16:39:47 +0900

    chapter36

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
+import { SignInResponse } from './dto/signInResponse';
+import type { JwtPayload } from './types/jwtPayload.type';
+import { User } from 'generated/prisma';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, password: string): Promise<User | null> {
+    const user = await this.userService.getUser(email);
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return user;
+    }
+    return null;
+  }
+
+  async signIn(user: User): Promise<SignInResponse> {
+    const payload: JwtPayload = { email: user.email, sub: user.id };
+    return { accessToken: this.jwtService.sign(payload), user };
+  }
+}
